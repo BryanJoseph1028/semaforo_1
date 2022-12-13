a74a28ce2c722a6fe3f79dded6e1e1e362a58020
bryan <bryancorado777.com>
2022-12-12 19:41:57 -0600

    first commit

---
+++ semaforo1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <unistd.h>
+sem_t semaphore1;
+
+void* haceralgo(void* arg) {
+//wait
+//critical session
+//signal
+
+}
+
+int main (){
+...
+return 0;
+}
